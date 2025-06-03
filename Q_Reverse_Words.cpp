3c547fee7515bc69c4c0c4847d4fb07e7e387c9b
Masrafi404 <mdmasrafi902@gmail.com>
2025-06-03 09:07:21 +0600

    reverse each word from a  sentence

---
+++ Q_Reverse_Words.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s;
+    getline(cin, s);
+
+    stringstream ss(s);
+
+    string world;
+
+    ss >> world;
+    reverse(world.begin(), world.end());
+    cout << world;
+
+    while (ss >> world)
+    {
+        reverse(world.begin(), world.end());
+        cout << " " << world;
+    }
+    cout << endl;
+    return 0;
+}
